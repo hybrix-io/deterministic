95dc4eba5420540bd4fff4ce0acd59c49f25ef84
Agent725 <agent725@725.be>
2021-04-11 17:24:22 +0200

    fix import secretPhrase, sanitize key management

--- modules/ardor/deterministic.js
+++ modules/ardor/deterministic.js
@@ -9,32 +9,38 @@ const ardor = require('./ardorjs');
 
 window.ardor = ardor;
 
+function mkPublicKey(seed) {
+  return ardor.secretPhraseToPublicKey(seed);
+}
+
+function mkAddress(seed) {
+  return ardor.publicKeyToAccountId(mkPublicKey(seed));
+}
+
 const wrapper = {
   // create deterministic public and private keys based on a seed
-  keys: data => {
-    const publicKey = ardor.secretPhraseToPublicKey(data.seed);
-    // simply pass the unique seed as secret phrase to the NXT library
-    return {
-      publicKey: publicKey,
-      secretPhrase: data.seed
-    };
-  },
+  keys: data => ({
+    secretPhrase: data.seed,            // simply pass the unique seed as secret phrase to the NXT library
+    publicKey: mkPublicKey(data.seed),
+    address: mkAddress(data.seed)
+  }),
 
-  // generate a unique wallet address from a given public key
-  address: data => {
-    const address = ardor.publicKeyToAccountId(data.publicKey);
-    return address;
-  },
-
-  // return public key
-  publickey: data => {
-    return data.publicKey;
-  },
+  // import private key in secretPhrase-format
+  importPrivate: data => ({
+    secretPhrase: data.privateKey,
+    publicKey: mkPublicKey(data.privateKey),
+    address: mkAddress(data.privateKey)
+  }),
 
   // return private key
-  privatekey: data => {
-    return data.secretPhrase;
-  },
+  privatekey: data => data.secretPhrase,
+
+  // generate a unique wallet address from a given public key
+  address: data => data.address,
+
+  // return public key
+  publickey: data => data.publicKey,
+
   // return deterministic transaction data
   transaction: data => {
     if (typeof data.unspent.unsignedTransactionBytes !== 'undefined') {
